11725afc2b971a16231590241db9784cbcd27224
BulbRight <79199683+BulbRight@users.noreply.github.com>
2025-03-23 17:20:28 +0100

    removed unshuffeling animation

--- src/entries/mcq.tsx
+++ src/entries/mcq.tsx
@@ -97,7 +97,7 @@ export default () => {
         return 'none';
     }
   });
-
+/*
   const [parent] = useAutoAnimate();
   useEffect(() => {
     if (back) {
@@ -108,7 +108,7 @@ export default () => {
     }
     return doNothing;
   }, [back]);
-
+*/
   const note = useField('note');
   const isMultipleChoice = answers.length > 1;
 
